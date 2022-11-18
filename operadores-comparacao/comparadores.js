65adafc808cfe2c6b50d2acb2ea7ab2b443a2437
Sávio Gomes da Silva <saviogomesdasilva@yahoo.com>
2022-11-17 21:26:39 -0300

    inciando tudo

---
+++ operadores-comparacao/comparadores.js
@@ -0,0 +1,19 @@
+/*operadores de comparação
+
+> maior que 
+< menor que
+>= manior que ou igual a 
+<=menor que ou igual a 
+== igualdade (valor)**********
+=== igualdade estrita (valor e tipo)
+!= diferente (valor) *********
+!== diferente estrito (valor e tipo)
+*/
+
+let dez = 10
+let quatro = 10
+if (dez >= quatro) {
+    console.log('sim')
+} else {
+    console.log('não')
+}
